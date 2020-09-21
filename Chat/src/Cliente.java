eac80e58a8a00c825604b5960ad046342d5a966a
sebasqr22 <63075293+sebasqr22@users.noreply.github.com>
2020-09-21 16:59:52 -0600

    Socket funcionado
    Haciendo modificaciones para crear el chat. Falta mover el servidor a la misma aplicacion cliente

--- Chat/src/Cliente.java
+++ Chat/src/Cliente.java
@@ -1,6 +1,9 @@
 import java.net.*;
 
 import java.awt.event.*;
+
+import java.io.DataOutputStream;
+
 import java.io.IOException;
 
 import javax.swing.*;
@@ -37,9 +40,21 @@ class MarcoConTexto extends JPanel{
 	
 	public MarcoConTexto() {
 		
+		nombre = new JTextField(5);
+		
+		add(nombre);
+		
+		numIp = new JTextField(5);
+		
+		add(numIp);
+		
 		JLabel tituloVentana = new JLabel("SERVICIO DE CHAT");
 		
 		add(tituloVentana);
+		
+		chat = new JTextArea(50,50);
+		
+		add(chat);
 		
 		cajaTexto1 = new JTextField(20);
 		
@@ -61,10 +76,24 @@ class MarcoConTexto extends JPanel{
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
 			
-			int numeroDePuerto = 1024;
+			//int numeroDePuerto = 26963;
 			
 			try {
-				Socket envioDeMensaje = new Socket("192.168.56.1", numeroDePuerto);
+				Socket envioDeMensaje = new Socket(InetAddress.getLocalHost(), 9999);
+				
+				Envio mensaje = new Envio();
+				
+				mensaje.setNombre(nombre.getText());
+				
+				mensaje.setNumIp(numIp.getText());
+				
+				mensaje.setCajaTexto1(cajaTexto1.getText());
+				
+				/*DataOutputStream datos = new DataOutputStream(envioDeMensaje.getOutputStream());
+				
+				datos.writeUTF(cajaTexto1.getText());
+				
+				datos.close();*/
 				
 			} catch (UnknownHostException e1) {
 				// TODO Auto-generated catch block
@@ -74,14 +103,46 @@ class MarcoConTexto extends JPanel{
 			} catch (IOException e1) {
 				// TODO Auto-generated catch block
 				
+				//numeroDePuerto++;
+				
 				System.out.println(e1.getMessage());
+				
 			}
-		}
-		
-		
+		}		
 	}
 	
-	private JTextField cajaTexto1;
+	private JTextField cajaTexto1, nombre, numIp;
 	
 	private JButton botonEnviar1;
+	
+	private JTextArea chat;
+}
+
+class Envio{
+	
+	private String nombre, numIp, cajaTexto1;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNumIp() {
+		return numIp;
+	}
+
+	public void setNumIp(String numIp) {
+		this.numIp = numIp;
+	}
+
+	public String getCajaTexto1() {
+		return cajaTexto1;
+	}
+
+	public void setCajaTexto1(String cajaTexto1) {
+		this.cajaTexto1 = cajaTexto1;
+	}
 }
--- Chat/src/Servidor.java
+++ Chat/src/Servidor.java
@@ -1,6 +1,10 @@
-import javax.swing.*;
-
 import java.awt.*;
+
+import java.io.*;
+
+import java.net.*;
+
+import javax.swing.*;
 
 public class Servidor {
 
@@ -14,7 +18,7 @@ public class Servidor {
 	}
 
 }
-class VentanaServidor extends JFrame{
+class VentanaServidor extends JFrame implements Runnable{
 	
 	public VentanaServidor() {
 		
@@ -24,16 +28,61 @@ class VentanaServidor extends JFrame{
 		
 		JPanel pantalla1 = new JPanel();
 		
-		//pantalla1.setLayout(new BorderLayout());
+		pantalla1.setLayout(new BorderLayout());
 		
-		JTextArea texto = new JTextArea();
+		texto = new JTextArea();
 		
 		pantalla1.add(texto, BorderLayout.CENTER);
 		
 		add(pantalla1);
 		
 		setVisible(true);
+		
+		Thread puertoAbierto = new Thread(this);
+		
+		puertoAbierto.start();
+		
 	}
 	
-	//private JTextArea texto;
+	private JTextArea texto;
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+		int contador = 0;
+		
+		try {
+			ServerSocket servidorMensajes = new ServerSocket(9999);
+			
+			while (true) {
+			
+				Socket socketServidor = servidorMensajes.accept();
+				
+				DataInputStream entradaDatos = new DataInputStream(socketServidor.getInputStream());
+				
+				String mensaje = entradaDatos.readUTF();
+				
+				if(contador==0) {
+					texto.append(mensaje);
+	
+					contador++;
+				}
+				
+				else {
+					texto.append("\n" + mensaje); 
+				}
+			
+				socketServidor.close();
+			
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			
+			//e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+	}
+	
 }
